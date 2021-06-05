8d97389b10ec13dd8564dd060fe4aa564b9d283d
Kanushka Gayan <kanushkanet@gmail.com>
2021-06-06 00:10:25 +0530

    update cover

--- resources/views/livewire/products/show-list.blade.php
+++ resources/views/livewire/products/show-list.blade.php
@@ -1,5 +1,15 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="mt-24 max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-between">
+            <div>
+                <div class="text-xl font-semibold">One Click Constructions</div>
+                <div class="text-5xl font-semibold mt-4">We Build Your Dream Home</div>
+            </div>
+            <div class="h-80 absolute top-20 right-64">
+                <iframe class="h-80" src="https://lottiefiles.com/iframe/64186-3x3-cubes-loader-1"></iframe>
+            </div>
+        </div>
+
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-6 mt-24">
             <div class="p-6 bg-white border-b border-gray-200">
                 <div class="grid grid-cols-2">
